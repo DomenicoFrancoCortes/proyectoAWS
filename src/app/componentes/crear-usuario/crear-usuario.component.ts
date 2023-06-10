b531217b8194b1bc424fe76b782d99f02c6f5fd7
Domenico Franco <domenicofranco.df@gmail.com>
2023-06-09 22:31:58 -0300

    Creacion usuario ahora si final

--- src/app/componentes/crear-usuario/crear-usuario.component.ts
+++ src/app/componentes/crear-usuario/crear-usuario.component.ts
@@ -22,17 +22,20 @@ export class CrearUsuarioComponent implements OnInit {
   }
 
   public signUpWithCognito() {
+  
+
     if (this.user && this.user.email && this.user.password) {
       this.cognitoService.signUp(this.user).then(() => {
         this.isConfirm = true;
         console.log('usuario creado');
       }).catch((error: any) => {
         this.displayAlert(error.message);
+        console.log('usuario no creado');
       })
     }
     else{
       this.displayAlert("informacion incorrecta");
-      
+      console.log('usuario incorrecto')
     }
   }
   public confirmSingUp(){
