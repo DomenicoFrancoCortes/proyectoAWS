987f03f80303932f8be31c8afc42ebc6d1e524a1
pblancoc <pablo_bc902@hotmail.com>
2023-06-18 21:23:52 -0300

    Se pone el JSON de DOME

--- src/app/services/destinos.service.ts
+++ src/app/services/destinos.service.ts
@@ -68,36 +68,10 @@ export class DestinosService {
   constructor(
     public http: HttpClient
   ) {
-    /*
-    this.destinoVerano = false;
-    this.destinoInvierno = false;
-    this.destinoOtono = false;
-    this.destinoPrimavera = false;
-    this.lugarElegido = 0;
-    this.actividadElegida = 0;
-    this.eventoElegido = 0;
-    this.lugares = [{
-      id: 0,
-      nombre: '',
-      descripcion: '',
-      imagen: '',
-      actividades: [],
-      eventos: []
-    }];
-    this.lugar ={
-      id: 0,
-      nombre: '',
-      descripcion: '',
-      imagen: '',
-      actividades: [],
-      eventos: []
-    }
-    */
   }
 
   obtenerDestinos() {
     console.log('haciendo GET a API destinos');
-    //return this.http.get('https://nextripjson.s3.amazonaws.com/json/destinos.json');
-    return this.http.get('https://proyectonube89.s3.amazonaws.com/json/destinos.json');
+    return this.http.get('https://nextripjson.s3.amazonaws.com/json/destinos_v2.json');
   }
 }
